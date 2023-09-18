a1bd7de3cfdd37b98b535ff864253bc7fa67294d
Andrew <asilkro@me.com>
2023-09-18 12:42:31 -0700

    WIP: Add two ways to encrypt data for SQL - AES256 in Security, DPAPI in Protected Data.

---
+++ Classes/ProtectedData.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Security.Cryptography;
+
+namespace ZenoBook.Classes
+{
+    /*
+     * Using this as a reference:
+    https://learn.microsoft.com/en-us/dotnet/api/system.security.cryptography.protecteddata?view=dotnet-plat-ext-7.0
+     */
+
+    public class ProtectedData
+    {
+        private static byte[] entropyArray = {9, 3, 1, 4, 12, 3, 14, 8, 0, 3};
+
+        public byte[] EncryptPw(string pw)
+        {
+            byte[] arrayPw = Encoding.Default.GetBytes(pw);
+            byte[] encryptedPw = System.Security.Cryptography.ProtectedData.Protect(arrayPw, entropyArray, DataProtectionScope.LocalMachine);
+            return encryptedPw;
+        }
+
+        public string DecryptPw(byte[] encryptedPw)
+        {
+            byte[] unencryptedPw = System.Security.Cryptography.ProtectedData.Unprotect(encryptedPw, entropyArray, DataProtectionScope.LocalMachine);
+            string pw = Encoding.Default.GetString(unencryptedPw);
+            return pw;
+        }
+
+    }
+
+    
+}
---
+++ Classes/Security.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ZenoBook.Classes
+{
+    public class Security
+    {
+        public static byte[] AES_Encrypt(byte[] bytesToBeEncrypted, byte[] passwordBytes)
+        {
+            byte[] encryptedBytes = null;
+
+            byte[] saltBytes = new byte[] { 8, 3, 2, 1, 0, 2, 1, 2, 1, 2, 5, 6, 7, 9, 4};
+
+            using (MemoryStream ms = new MemoryStream())
+            {
+                using (Aes AES = new AesManaged())
+                {
+                    AES.KeySize = 256;
+                    AES.BlockSize = 128;
+
+                    var key = new Rfc2898DeriveBytes(passwordBytes, saltBytes, 1000);
+                    AES.Key = key.GetBytes(AES.KeySize / 8);
+                    AES.IV = key.GetBytes(AES.BlockSize / 8);
+
+                    AES.Mode = CipherMode.CBC;
+
+                    using (var cs = new CryptoStream(ms, AES.CreateEncryptor(), CryptoStreamMode.Write))
+                    {
+                        cs.Write(bytesToBeEncrypted, 0, bytesToBeEncrypted.Length);
+                        cs.Close();
+                    }
+                    encryptedBytes = ms.ToArray();
+                }
+            }
+
+            return encryptedBytes;
+        }
+
+        public static byte[] AES_Decrypt(byte[] bytesToBeDecrypted, byte[] passwordBytes)
+        {
+            byte[] decryptedBytes = null;
+
+            byte[] saltBytes = new byte[] { 8, 3, 2, 1, 0, 2, 1, 2, 1, 2, 5, 6, 7, 9, 4 };
+
+            using (MemoryStream ms = new MemoryStream())
+            {
+                using (Aes AES = new AesManaged())
+                {
+                    AES.KeySize = 256;
+                    AES.BlockSize = 128;
+
+                    var key = new Rfc2898DeriveBytes(passwordBytes, saltBytes, 1000);
+                    AES.Key = key.GetBytes(AES.KeySize / 8);
+                    AES.IV = key.GetBytes(AES.BlockSize / 8);
+
+                    AES.Mode = CipherMode.CBC;
+
+                    using (var cs = new CryptoStream(ms, AES.CreateDecryptor(), CryptoStreamMode.Write))
+                    {
+                        cs.Write(bytesToBeDecrypted, 0, bytesToBeDecrypted.Length);
+                        cs.Close();
+                    }
+                    decryptedBytes = ms.ToArray();
+                }
+            }
+
+            return decryptedBytes;
+        }
+    }
+}
